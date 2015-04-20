828af31bba455f4fdafd7d209a622b705781d232
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2015-04-21 00:59:07 +0200

    moar fixes

--- src/simplex.ml
+++ src/simplex.ml
@@ -1,48 +1,51 @@
 open Field
 open Dictionary
 
-module Make(F:FIELD) = struct
-
 type 'a t = Empty of 'a Dictionary.t | Unbounded of ('a Dictionary.t)*int | Opt of 'a Dictionary.t
 
-(************* Global functions ****************)
+module Make(F:FIELD) = struct
 
-let add_rows r1 r2 c = (* r1 <- r1 + c*r2 *)
-  assert Array.(length r1.body = length r2.body);
-  Array.iteri
-    (fun n x -> r1.body.(n) <- r1.body.(n) + c*x)
-    r2.body;
-  r1.const <- r1.const + c*r2.const
+  (************* Global functions ****************)
 
-exception Found of int
-
-let array_find arr f = (* Some n if arr.[n] is the first elt of arr that verifies f x, None otherwise *)
-  try
+  let add_rows r1 r2 c = (* r1 <- r1 + c*r2 *)
+    assert Array.(length r1.body = length r2.body);
     Array.iteri
-      (fun n x -> if f x then raise (Found n))
-      arr;
+      (fun n x -> r1.body.(n) <- r1.body.(n) + c*x)
+      r2.body;
+    r1.const <- r1.const + c*r2.const
+
+  exception Found of int
+
+  let array_find f arr = (* Some n if arr.[n] is the first elt of arr that verifies f x, None otherwise *)
+    try
+      Array.iteri
+        (fun n x -> if f x then raise (Found n))
+        arr;
     None
-  with Found n -> Some n
+    with Found n -> Some n
 
-let array_doublemap arr1 arr2 ?(except = -1) f = (* arr1.(n) <- f arr1.(n) arr2.(n), except for n *)
-  assert (Array.length arr1 == Array.length arr2);
-  Array.iteri
-    (fun n x -> if n <> except then arr1.(n) <- f arr1.(n) x)
-    arr2
+  let array_doublemap arr1 arr2 ?(except = -1) f = (* arr1.(n) <- f arr1.(n) arr2.(n), except for n *)
+    assert (Array.length arr1 == Array.length arr2);
+    Array.iteri
+      (fun n x -> if n <> except then arr1.(n) <- f arr1.(n) x)
+      arr2
 
-let partial_copy arr n = (* return a copy of array arr without entry n *)
-  assert (n >= 0 && n < Array.length arr);
-  let new_arr = Array.init
-      (Array.length arr - 1)
-      (fun i ->
-         if i<n then
-           arr.(i)
-         else
-           arr.(i+1)) in
-  new_arr
+  let array_iter ?(except = -1) f =
+    Array.iteri (fun n x -> if n <> except then f x)
+
+  let partial_copy arr n = (* return a copy of array arr without entry n *)
+    assert (n >= 0 && n < Array.length arr);
+    let new_arr = Array.init
+        (Array.length arr - 1)
+        (fun i ->
+           if i<n then
+             arr.(i)
+           else
+             arr.(i+1)) in
+    new_arr
 
 
-    (* let (a,b) = Array.fold_left
+  (* let (a,b) = Array.fold_left
       (fun (m,pos) x ->
         if n == m then
           (m+1,pos)
@@ -52,47 +55,49 @@ let partial_copy arr n = (* return a copy of array arr without entry n *)
             (m+1,pos+1)
           end)
       (0,0) arr in
-  assert (a <> b);*)
+     assert (a <> b);*)
 
-(************* Simplex without first phase ****************)
+  (************* Simplex without first phase ****************)
 
-let choose_entering dict = (* Some v if dict.vars.(v) is the entering variable, None if no entering variable *)
-  array_find dict.coeffs.body (fun x -> Field.(compare x F.zero) > 0)
+  let choose_entering dict = (* Some v if dict.vars.(v) is the entering variable, None if no entering variable *)
+    array_find (fun x -> F.(compare x F.zero) > 0) dict.coeffs.body
 
-let choose_leaving ent dict = (* Some v if dict.rows.(v).head is the leaving variable, None if unbounded *)
-  let (_,max_var,_,denum) =
+  let choose_leaving ent dict = (* Some v if dict.rows.(v).head is the leaving variable, None if unbounded *)
+    let (_, max_var, _, denum) =
     Array.fold_left
-      (fun (pos,pos_temp,num,denum) r ->
-         let (num_r,denum_r) = (r.const,r.body.(ent)) in
-         if Field.(compare (num_r * denum_r) Field.zero) < 0 && Field.(compare (num_r * denum) (denum_r * num)) >= 0 then (** marche aussi pour 1st phase ?*)
-           (pos+1,pos,num_r,denum_r)
+      (fun (pos, pos_temp, num, denum) r ->
+         let (num_r, denum_r) = (r.const,r.body.(ent)) in
+         if F.(compare (num_r * denum_r) F.zero) < 0 && F.(compare (num_r * denum) (denum_r * num)) >= 0 then (** marche aussi pour 1st phase ?*)
+           (pos+1, pos, num_r, denum_r)
          else
-           (pos+1,pos_temp,num,denum))
-      (0,0,F.zero,F.zero) dict.rows in
-  if F.(compare denum F.zero) > 0 then
-    Some max_var
-  else
-    None
+           (pos+1, pos_temp, num, denum))
+      (0 , 0, F.zero, F.zero)
+      dict.rows in
+    if F.(compare denum F.zero) > 0 then
+      Some max_var
+    else
+      None
 
-let update_row ent lea r dict = (* row lea has been updated according to ent. now, update row r *)
-  let coeff = r.body.(ent) in
-    r.body.(ent) <- Field.zero;
-    array_doublemap r.body dict.rows.(lea).body (fun c1 c2 -> c1 + (coeff * c2));
-    r.const <- r.const + (coeff * dict.rows.(lea).const)
+  let update_row ent lea_r r = (* row lea_r has been updated according to ent. now, update row r *)
+    let coeff = r.body.(ent) in
+    r.body.(ent) <- F.zero;
+    array_doublemap r.body lea_r.body (fun c1 c2 -> F.(c1 + (coeff * c2)));
+    r.const <- F.(r.const + (coeff * lea_r.const))
 
-let update_dict ent lea dict = (* update all the dictionary, excepting row lea and vars *)
-  array_doublemap dict.rows dict.rows ~except:lea (fun r _ -> update_row ent lea r dict);
-  update_row ent lea dict.coeffs dict
+  let update_dict ent lea dict = (* update all the dictionary, excepting row lea and vars *)
+    let lea_r = dict.rows.(lea) in
+    array_iter ~except:lea (fun r -> update_row ent lea_r r) dict.rows;
+    update_row ent lea_r dict.coeffs
 
-let pivot ent lea dict = (* Pivot colum ent and row lea *)
-  let ent_var = dict.vars.(ent) in (* name of the entering variable *)
-  let piv_row = dict.rows.(lea) in (* row to be pivot *)
-  let coeff = piv_row.body.(ent) in (* coeff of the entering variable into piv_row *)
-      dict.vars.(ent) <- dict.heads.(lea);
-      dict.heads.(lea) <- ent_var;
-      piv_row.body.(ent) <- neg F.one;
-      array_doublemap piv_row piv_row (fun x _ -> x / (neg coeff));
-      update_dict ent lea dict (* update the other rows + the objective *)
+  let pivot ent lea dict = (* Pivot colum ent and row lea *)
+    let ent_var = dict.vars.(ent) in (* name of the entering variable *)
+    let piv_row = dict.rows.(lea) in (* row to be pivot *)
+    let coeff = piv_row.body.(ent) in (* coeff of the entering variable into piv_row *)
+    dict.vars.(ent) <- dict.heads.(lea);
+    dict.heads.(lea) <- ent_var;
+    piv_row.body.(ent) <- F.(neg one);
+    Array.iteri (fun i x -> piv_row.body.(i) <- F.(x / (neg coeff))) piv_row.body; (*** Vérifie ici***)
+    update_dict ent lea dict (* update the other rows + the objective *)
 
 let rec pivots dict = (* Pivots the dictionnary until being blocked *)
   match choose_entering dict with
@@ -174,7 +179,7 @@ let first_phase dict = (* Simplex when first phase needed *)
           match pivots dict with
             | Opt dict | Unbounded (dict,ent) ->
                 let dict_proj = project coeffs_init heads_init vars_init aux_var dict in (* projection of the dictionary, remove the auxiliary variable *)
-                if Field.compare(dict.coeffs.const Field.zero) < 0 then
+                if F.compare(dict.coeffs.const F.zero) < 0 then
                   Empty dict_proj
                 else
                   pivots dict_proj
