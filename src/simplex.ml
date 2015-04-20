7d3d212dc8dea25765016169be140b9b8b407f50
Maxime Lesourd <maxime.lesourd@ens-lyon.fr>
2015-04-21 00:32:40 +0200

    first fixes and optimisations

--- src/simplex.ml
+++ src/simplex.ml
@@ -1,52 +1,48 @@
 open Field
 open Dictionary
 
-module Make(Field:FIELD) = struct
+module Make(F:FIELD) = struct
 
 type 'a t = Empty of 'a Dictionary.t | Unbounded of ('a Dictionary.t)*int | Opt of 'a Dictionary.t
 
 (************* Global functions ****************)
 
 let add_rows r1 r2 c = (* r1 <- r1 + c*r2 *)
-  let _ =
-    Array.fold_left
-      (fun n x ->
-        r1.body.(n) <- r1.body.(n) + c*x;
-        n+1)
-      0 r2.body in
+  assert Array.(length r1.body = length r2.body);
+  Array.iteri
+    (fun n x -> r1.body.(n) <- r1.body.(n) + c*x)
+    r2.body;
   r1.const <- r1.const + c*r2.const
 
 exception Found of int
 
 let array_find arr f = (* Some n if arr.[n] is the first elt of arr that verifies f x, None otherwise *)
   try
-    let _ =
-      Array.fold_left
-        (fun n x ->
-          if f x then
-            raise (Found n)
-          else
-            n+1)
-        0 arr in
+    Array.iteri
+      (fun n x -> if f x then raise (Found n))
+      arr;
     None
   with Found n -> Some n
 
 let array_doublemap arr1 arr2 ?(except = -1) f = (* arr1.(n) <- f arr1.(n) arr2.(n), except for n *)
   assert (Array.length arr1 == Array.length arr2);
-  let _ =
-    Array.fold_left
-      (fun n x ->
-        if n <> except then
-          arr1.(n) <- f arr1.(n) x;
-        n+1
-      )
-      0 arr2 in
-  ()
+  Array.iteri
+    (fun n x -> if n <> except then arr1.(n) <- f arr1.(n) x)
+    arr2
 
 let partial_copy arr n = (* return a copy of array arr without entry n *)
-  let new_arr = Array.make (Array.length arr - 1) arr.(0) in
-  let (a,b) =
-    Array.fold_left
+  assert (n >= 0 && n < Array.length arr);
+  let new_arr = Array.init
+      (Array.length arr - 1)
+      (fun i ->
+         if i<n then
+           arr.(i)
+         else
+           arr.(i+1)) in
+  new_arr
+
+
+    (* let (a,b) = Array.fold_left
       (fun (m,pos) x ->
         if n == m then
           (m+1,pos)
@@ -56,13 +52,12 @@ let partial_copy arr n = (* return a copy of array arr without entry n *)
             (m+1,pos+1)
           end)
       (0,0) arr in
-  assert (a <> b);
-  new_arr
+  assert (a <> b);*)
 
 (************* Simplex without first phase ****************)
 
 let choose_entering dict = (* Some v if dict.vars.(v) is the entering variable, None if no entering variable *)
-  array_find dict.coeffs.body (fun x -> Field.(compare x Field.zero) > 0)
+  array_find dict.coeffs.body (fun x -> Field.(compare x F.zero) > 0)
 
 let choose_leaving ent dict = (* Some v if dict.rows.(v).head is the leaving variable, None if unbounded *)
   let (_,max_var,_,denum) =
@@ -73,8 +68,8 @@ let choose_leaving ent dict = (* Some v if dict.rows.(v).head is the leaving var
            (pos+1,pos,num_r,denum_r)
          else
            (pos+1,pos_temp,num,denum))
-      (0,0,Field.zero,Field.zero) dict.rows in
-  if Field.(compare denum Field.zero) > 0 then
+      (0,0,F.zero,F.zero) dict.rows in
+  if F.(compare denum F.zero) > 0 then
     Some max_var
   else
     None
@@ -95,7 +90,7 @@ let pivot ent lea dict = (* Pivot colum ent and row lea *)
   let coeff = piv_row.body.(ent) in (* coeff of the entering variable into piv_row *)
       dict.vars.(ent) <- dict.heads.(lea);
       dict.heads.(lea) <- ent_var;
-      piv_row.body.(ent) <- neg Field.one;
+      piv_row.body.(ent) <- neg F.one;
       array_doublemap piv_row piv_row (fun x _ -> x / (neg coeff));
       update_dict ent lea dict (* update the other rows + the objective *)
 
@@ -117,10 +112,10 @@ let auxiliary_dict aux_var dict = (* Start of first phase: add an auxiliary vari
   let aux_dic =
     { vars = Array.append dict.var [|aux_var|]
     ; heads = dict.heads
-    ; coeffs = Array.append (Array.make (Array.length dict.coeffs) Field.zero) [|neg Field.one|]
+    ; coeffs = Array.append (Array.make (Array.length dict.coeffs) F.zero) [|neg F.one|]
     ; rows = dict.rows
     } in
-  array_doublemap aux_dic.rows aux_dic.rows (fun r _ -> Array.append r [|Field.one|]);
+  array_doublemap aux_dic.rows aux_dic.rows (fun r _ -> Array.append r [|F.one|]);
   aux_dic
 
 type place = Basic of int | Non_basic of int
@@ -128,7 +123,7 @@ type place = Basic of int | Non_basic of int
 module Vars_map = Map.Make(struct type t = var_id*int let compare = compare end) (* place of each variable in the initial dictionary. If v -> Basic n, then heads.(n) = v. If v -> Non_basic n then coeffs.(n) = v *)
 
 let save_place heads_init vars_init =
-  let save_basic = 
+  let save_basic =
     Array.fold_left
       (fun (pos,m) v_basic ->
         (pos+1,Vars_map.add v_basic (Basic pos) m))
@@ -138,7 +133,7 @@ let save_place heads_init vars_init =
     (fun (pos',m') v_nonbasic ->
       (pos'+1,Vars_map.add v_nonbasic (Non_basic pos') m'))
     (0,save_basic)
-    vars_init    
+    vars_init
 
 let rec project_var v coeff places coeffs_init vars_init dict =
   if coeff <> 0 then
@@ -189,7 +184,7 @@ let first_phase dict = (* Simplex when first phase needed *)
 (************* Final function ****************)
 
 let simplex dict = (* Apply the whole simplex *)
-  match array_find dict.heads (fun x -> Field.(compare x Field.zero) < 0) with
+  match array_find dict.heads (fun x -> F.(compare x F.zero) < 0) with
     | Some _ -> first_phase dict
     | None -> pivots dict
 
